657b2fdf07fe9a04e82dd161e415d699ae5ca096
szeplaki <eva.szeplaki@gmail.com>
2023-11-16 02:12:40 +0100

    add one more loop to have one commit more

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,9 @@ public class Main {
         for (int i = 0; i < 5; i++) {
             System.out.println(i);
         }
+        for (int i = 5; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 
 }
